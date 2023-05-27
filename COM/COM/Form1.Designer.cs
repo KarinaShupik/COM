ab46e5a65d4f9aefbf7b9ce208bec153ca4bed60
Karina <105536477+KarinaShupik@users.noreply.github.com>
2023-05-27 11:10:19 +0300

    COM

---
+++ COM/COM/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace COM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnGenerateDocument = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSelectTemplate = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTemplatePath = new System.Windows.Forms.TextBox();
+            this.btnSaveAs = new System.Windows.Forms.Button();
+            this.txtSave = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtNameSurname = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(395, 70);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(195, 22);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(395, 102);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(195, 22);
+            this.txtSurname.TabIndex = 1;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(395, 134);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(195, 22);
+            this.txtPhone.TabIndex = 2;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(395, 168);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(195, 22);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // btnGenerateDocument
+            // 
+            this.btnGenerateDocument.Location = new System.Drawing.Point(54, 312);
+            this.btnGenerateDocument.Name = "btnGenerateDocument";
+            this.btnGenerateDocument.Size = new System.Drawing.Size(195, 36);
+            this.btnGenerateDocument.TabIndex = 4;
+            this.btnGenerateDocument.Text = "Згенерувати документ";
+            this.btnGenerateDocument.UseVisualStyleBackColor = true;
+            this.btnGenerateDocument.Click += new System.EventHandler(this.btnGenerateDocument_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Введіть ім\'я";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Введіть прізвище";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Введіть номер телефону";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Введіть почту";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(51, 262);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(285, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Введіть шлях до вашого файлу (результат)";
+            // 
+            // btnSelectTemplate
+            // 
+            this.btnSelectTemplate.Location = new System.Drawing.Point(267, 312);
+            this.btnSelectTemplate.Name = "btnSelectTemplate";
+            this.btnSelectTemplate.Size = new System.Drawing.Size(195, 36);
+            this.btnSelectTemplate.TabIndex = 11;
+            this.btnSelectTemplate.Text = "Обрати шаблон";
+            this.btnSelectTemplate.UseVisualStyleBackColor = true;
+            this.btnSelectTemplate.Click += new System.EventHandler(this.btnSelectTemplate_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(51, 234);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(222, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Введіть шлях до вашого шаблону ";
+            // 
+            // txtTemplatePath
+            // 
+            this.txtTemplatePath.Location = new System.Drawing.Point(395, 228);
+            this.txtTemplatePath.Name = "txtTemplatePath";
+            this.txtTemplatePath.Size = new System.Drawing.Size(195, 22);
+            this.txtTemplatePath.TabIndex = 13;
+            // 
+            // btnSaveAs
+            // 
+            this.btnSaveAs.Location = new System.Drawing.Point(482, 312);
+            this.btnSaveAs.Name = "btnSaveAs";
+            this.btnSaveAs.Size = new System.Drawing.Size(108, 36);
+            this.btnSaveAs.TabIndex = 14;
+            this.btnSaveAs.Text = "Зберегти як";
+            this.btnSaveAs.UseVisualStyleBackColor = true;
+            this.btnSaveAs.Click += new System.EventHandler(this.btnSaveAs_Click);
+            // 
+            // txtSave
+            // 
+            this.txtSave.Location = new System.Drawing.Point(395, 256);
+            this.txtSave.Name = "txtSave";
+            this.txtSave.Size = new System.Drawing.Size(195, 22);
+            this.txtSave.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(51, 203);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(109, 16);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Введіть ініціали";
+            // 
+            // txtNameSurname
+            // 
+            this.txtNameSurname.Location = new System.Drawing.Point(395, 196);
+            this.txtNameSurname.Name = "txtNameSurname";
+            this.txtNameSurname.Size = new System.Drawing.Size(195, 22);
+            this.txtNameSurname.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 381);
+            this.Controls.Add(this.txtNameSurname);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtSave);
+            this.Controls.Add(this.btnSaveAs);
+            this.Controls.Add(this.txtTemplatePath);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnSelectTemplate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGenerateDocument);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnGenerateDocument;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSelectTemplate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtTemplatePath;
+        private System.Windows.Forms.Button btnSaveAs;
+        private System.Windows.Forms.TextBox txtSave;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtNameSurname;
+    }
+}
+
---
+++ COM/COM/Form1.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Office.Interop.Word;
+using System.Reflection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace COM
+{
+    public partial class Form1 : Form
+    {
+        private string templatePath = "";
+        private string outputiPath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ReplacePlaceholder(string placeholder, string value, Document wordDoc)
+        {
+            var range = wordDoc.Content;
+            range.Find.ClearFormatting();
+            range.Find.Execute(FindText: placeholder, ReplaceWith: value);
+        }
+
+
+        private void btnSelectTemplate_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Word Document (*.dotx)|*.dotx";
+            openFileDialog.Title = "Select a Word Template File";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                templatePath = openFileDialog.FileName;
+                txtTemplatePath.Text = templatePath;
+            }
+        }
+
+        private void btnSaveAs_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Word Document (*.docx)|*.docx";
+            saveFileDialog.Title = "Select a Word Template File";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                outputiPath = saveFileDialog.FileName;
+                txtSave.Text = outputiPath;
+            }
+
+        }
+
+        private void btnGenerateDocument_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(templatePath))
+            {
+                Microsoft.Office.Interop.Word.Application wordApp = new
+               Microsoft.Office.Interop.Word.Application();
+                Document wordDoc = null;
+                try
+                {
+                    Object missingObj = System.Reflection.Missing.Value;
+                    Object trueObj = true;
+                    Object falseObj = false;
+                    Object templatePathObj = templatePath;
+                    wordDoc = wordApp.Documents.Add(ref templatePathObj, ref missingObj, ref
+                   missingObj, ref missingObj);
+                    wordDoc.Activate();
+                    ReplacePlaceholder("[name]", txtName.Text, wordDoc);
+                    ReplacePlaceholder("[surname]", txtSurname.Text, wordDoc);
+                    ReplacePlaceholder("[phone]", txtPhone.Text, wordDoc);
+                    ReplacePlaceholder("[email]", txtEmail.Text, wordDoc);
+
+
+                    wordApp.Visible = true;
+                    string outputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+$"{txtNameSurname.Text} Certificate.docx");
+
+                    wordDoc.SaveAs2(outputPath);
+                    MessageBox.Show("Certificate generated successfully.", "Success",
+                   MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error occurred: {ex.Message}", "Error", MessageBoxButtons.OK,
+                   MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    wordDoc.Close();
+                    wordApp.Quit();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a Word template file first.", "Error",
+               MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+    }
+
